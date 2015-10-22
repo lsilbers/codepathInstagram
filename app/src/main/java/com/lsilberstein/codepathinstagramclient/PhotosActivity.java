3b22fd3f894bd4b93c1a039e1f3a24a9f6b652a4
Leo Silberstein <lsilberstein@groupon.com>
2015-10-21 22:06:43 -0700

    finished basic app

--- app/src/main/java/com/lsilberstein/codepathinstagramclient/PhotosActivity.java
+++ app/src/main/java/com/lsilberstein/codepathinstagramclient/PhotosActivity.java
@@ -1,16 +1,66 @@
 package com.lsilberstein.codepathinstagramclient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class PhotosActivity extends AppCompatActivity {
+    private ArrayList<Photo> photos;
+    private PhotoAdapter aPhotos;
+    private ListView lvPhotos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
+
+        // get the view
+        lvPhotos = (ListView) findViewById(R.id.lvPhotos);
+
+        photos = new ArrayList<>();
+        // construct the adapter
+        aPhotos = new PhotoAdapter(this, photos);
+        // set the adapter for the view
+        lvPhotos.setAdapter(aPhotos);
+        // retrieve the data to display
+        fetchPopularPhotos();
+
+    }
+
+    private void fetchPopularPhotos() {
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(InstagramClientUtils.getPopularUrl(), new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    Log.i("PA", "Successfully retrieved data");
+                    ArrayList<Photo> returnedPhotos = InstagramClientUtils.toPhotos(response);
+                    aPhotos.addAll(returnedPhotos);
+                    Log.i("PA", "Added Photos");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
     }
 
     @Override
